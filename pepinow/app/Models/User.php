d041dd29ad37d2f574fcdf74e84c033a6d0c6c86
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-03-22 17:34:13 +0100

    terminer login signup et travailler sur reset password

--- pepinow/app/Models/User.php
+++ pepinow/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
